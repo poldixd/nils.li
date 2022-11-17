13387496cc424b0d8d92f8366f668c79fdf2802b
Nils Poltmann <nils@poltmann.com>
2022-11-17 14:59:10 +0100

    Added valetTls to config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        host: 'nils.test'
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
+            valetTls: 'nils.test',
             refresh: true,
         }),
     ],
