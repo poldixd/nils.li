3591de7ef1c5495b84b04bab156a7eca0f172212
Nils Poltmann <nils@poltmann.com>
2023-11-28 14:38:29 +0100

    Added "/hilfe" Route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'index')->name('index');
+Route::permanentRedirect('/hilfe', 'https://github.com/poldixd/rustdesk/releases/download/nightly/rustdesk-1.2.4-x86_64.exe');
 Route::view('/impressum', 'impressum')->name('impressum');
 Route::view('/datenschutzerklaerung', 'datenschutzerklaerung')->name('datenschutzerklaerung');
