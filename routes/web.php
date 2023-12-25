33bc8d96ff06f2bd6f286b77b122a00f31414e85
Nils Poltmann <nils@poltmann.com>
2023-12-25 12:10:41 +0100

    Revert "Added "/hilfe" Route"
    This reverts commit 3591de7ef1c5495b84b04bab156a7eca0f172212

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'index')->name('index');
-Route::permanentRedirect('/hilfe', 'https://github.com/poldixd/rustdesk/releases/download/nightly/rustdesk-1.2.4-x86_64.exe');
 Route::view('/impressum', 'impressum')->name('impressum');
 Route::view('/datenschutzerklaerung', 'datenschutzerklaerung')->name('datenschutzerklaerung');
